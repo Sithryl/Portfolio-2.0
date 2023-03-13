ca748ed382a3e8531f7c0c0dab68f4fc097e959e
Sithryl <david@tgsummit.com>
2023-03-13 00:09:07 -0400

    added resume download button

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,25 +3,25 @@ import React from "react";
 export default function Contact() {
   return (
     <div>
-      <div id="contact" class="relative py-20 md:py-24 bg-gray-800">
-        <div class="container xl:max-w-6xl mx-auto px-4">
+      <div id="contact" className="relative py-20 md:py-24 bg-gray-800">
+        <div className="container xl:max-w-6xl mx-auto px-4">
           {/* row start */}
 
-          <div class="flex flex-wrap flex-row -mx-4">
+          <div className="flex flex-wrap flex-row -mx-4">
             <div
-              class="flex-shrink max-w-full w-full px-4 lg:w-5/12 lg:pr-12 wow fadeInLeft"
+              className="flex-shrink max-w-full w-full px-4 lg:w-5/12 lg:pr-12 wow fadeInLeft"
               data-wow-duration="1s"
               data-wow-delay=".1s"
             >
-              <div class="p-12 shadow rounded bg-white">
+              <div className="p-12 shadow rounded bg-white">
                 <div className="flex">
                   {/* email */}
-                  <div class="mb-6">
-                    <div class="flex flex-wrap flex-row -mx-4">
-                      <div class="flex-shrink max-w-full w-1/6 px-4 text-center">
+                  <div className="mb-8">
+                    <div className="flex flex-wrap flex-row -mx-4">
+                      <div className="flex-shrink max-w-full w-1/6 px-4 text-center">
                         {/* icon */}
                         <svg
-                          className="mt-2 text-3xl text-blue-700 inline-block"
+                          className="mt-2 text-3xl text-blue-700 inline-block transform ltr:rotate-0 rtl:-rotate-90"
                           xmlns="http://www.w3.org/2000/svg"
                           width="32"
                           height="32"
@@ -29,20 +29,20 @@ export default function Contact() {
                         >
                           <path d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z" />
                         </svg>
-                        <div class="flex-shrink max-w-full w-10/12 px-4">
-                          <p class="text-lg leading-normal mb-2 font-semibold text-gray-800">
+                        <div className="flex-shrink max-w-full w-10/12 px-4">
+                          <p className="text-lg leading-normal mb-2 font-semibold text-gray-800">
                             Email:
                           </p>
-                          <p>david@tgsummit.com</p>
+                          <p className="mb-6">david@tgsummit.com</p>
                         </div>
                       </div>
                     </div>
                     {/* phone */}
-                    <div class="mb-6">
-                      <div class="flex flex-wrap flex-row -mx-4">
-                        <div class="flex-shrink max-w-full w-1/6 px-4 text-center">
+                    <div className="mb-8">
+                      <div className="flex flex-wrap flex-row -mx-4">
+                        <div className="flex-shrink max-w-full w-1/6 px-4 text-center">
                           <svg
-                            class="mt-2 text-3xl text-blue-700 inline-block transform ltr:rotate-0 rtl:-rotate-90"
+                            className="mt-2 text-3xl text-blue-700 inline-block transform ltr:rotate-0 rtl:-rotate-90"
                             xmlns="http://www.w3.org/2000/svg"
                             width="32"
                             height="32"
@@ -54,77 +54,90 @@ export default function Contact() {
                             />
                           </svg>
                         </div>
-                        <div class="flex-shrink max-w-full w-10/12 px-4">
-                          <p class="text-lg leading-normal mb-2 font-semibold text-gray-800">
+                        <div className="flex-shrink max-w-full w-10/12 px-4">
+                          <p className="text-lg leading-normal mb-2 font-semibold text-gray-800">
                             Call:
                           </p>
-                          <p class="mb-0">+1 704 696 6093</p>
+                          <p className="mb-2">+1 704 696 6093</p>
                         </div>
                       </div>
                     </div>
-                    
+                    {/* resume */}
+                    <div>
+                      <a
+                        href="../../../src/assets/2021_David_M_McWhirter.pdf"
+                        download="Resume"
+                      >
+                        <button
+                          type="button"
+                          className="py-3 px-5 inline-block text-center mb-3 rounded-md leading-normal text-violet-900 bg-violet-900 border border-violet-900 hover:text-white hover:bg-violet-900 hover:ring-0 hover:border-blue-800 focus:bg-blue-800 focus:border-blue-800 focus:outline-none focus:ring-0"
+                        >
+                          Download Resume
+                        </button>
+                      </a>
+                    </div>
                   </div>
                   <div
-                    class="flex-shrink max-w-full w-full px-4 lg:w-7/12 lg:pl-0 mt-12 lg:mt-0 wow fadeInRight"
+                    className="flex-shrink max-w-full w-full px-4 lg:w-7/12 lg:pl-0 mt-12 lg:mt-0 wow fadeInRight"
                     data-wow-duration="1s"
                     data-wow-delay=".3s"
                   >
-                    <div class="w-100 p-12 shadow rounded bg-white">
+                    <div className="w-100 p-12 shadow rounded bg-white">
                       {/* contact form */}
                       <form action="mailto: david@tgsummit.com">
-                        <div class="flex flex-wrap flex-row -mx-4">
-                          <div class="flex-shrink w-full max-w-full md:w-1/2 px-4 mb-6">
-                            <label class="inline-block mb-2" for="name">
+                        <div className="flex flex-wrap flex-row -mx-4">
+                          <div className="flex-shrink w-full max-w-full md:w-1/2 px-4 mb-6">
+                            <label className="inline-block mb-2" for="name">
                               Your Name
                             </label>
                             <input
                               type="text"
                               name="name"
-                              class="w-full leading-5 relative py-3 px-5 rounded text-gray-800 bg-white border border-gray-300 overflow-x-auto focus:outline-none focus:border-gray-400 focus:ring-0"
+                              className="w-full leading-5 relative py-3 px-5 rounded text-gray-800 bg-white border border-gray-300 overflow-x-auto focus:outline-none focus:border-gray-400 focus:ring-0"
                               id="name"
                             />
-                            <div class="validate"></div>
+                            <div className="validate"></div>
                           </div>
-                          <div class="flex-shrink w-full max-w-full md:w-1/2 px-4 mb-6">
-                            <label class="inline-block mb-2" for="email">
+                          <div className="flex-shrink w-full max-w-full md:w-1/2 px-4 mb-6">
+                            <label className="inline-block mb-2" for="email">
                               Your Email
                             </label>
                             <input
                               type="email"
-                              class="w-full leading-5 relative py-3 px-5 rounded text-gray-800 bg-white border border-gray-300 overflow-x-auto focus:outline-none focus:border-gray-400 focus:ring-0"
+                              className="w-full leading-5 relative py-3 px-5 rounded text-gray-800 bg-white border border-gray-300 overflow-x-auto focus:outline-none focus:border-gray-400 focus:ring-0"
                               name="email"
                               id="email"
                             />
-                            <div class="validate"></div>
+                            <div className="validate"></div>
                           </div>
                         </div>
-                        <div class="mb-6">
-                          <label class="inline-block mb-2" for="subject">
+                        <div className="mb-6">
+                          <label className="inline-block mb-2" for="subject">
                             Subject
                           </label>
                           <input
                             type="text"
-                            class="w-full leading-5 relative py-3 px-5 rounded text-gray-800 bg-white border border-gray-300 overflow-x-auto focus:outline-none focus:border-gray-400 focus:ring-0"
+                            className="w-full leading-5 relative py-3 px-5 rounded text-gray-800 bg-white border border-gray-300 overflow-x-auto focus:outline-none focus:border-gray-400 focus:ring-0"
                             name="subject"
                             id="subject"
                           />
-                          <div class="validate"></div>
+                          <div className="validate"></div>
                         </div>
-                        <div class="mb-6">
-                          <label class="inline-block mb-2" for="messages">
+                        <div className="mb-6">
+                          <label className="inline-block mb-2" for="messages">
                             Message
                           </label>
                           <textarea
-                            class="w-full leading-5 relative py-3 px-5 rounded text-gray-800 bg-white border border-gray-300 overflow-x-auto focus:outline-none focus:border-gray-400 focus:ring-0"
+                            className="w-full leading-5 relative py-3 px-5 rounded text-gray-800 bg-white border border-gray-300 overflow-x-auto focus:outline-none focus:border-gray-400 focus:ring-0"
                             name="message"
                             rows="10"
                             id="messages"
                           ></textarea>
-                          <div class="validate"></div>
+                          <div className="validate"></div>
                         </div>
-                        <div class="text-center mb-6">
+                        <div className="text-center mb-6">
                           <button
-                            class="py-3 px-5 -ml-1 rounded-md leading-5 text-gray-100 bg-red-800 border border-red-700 hover:text-white hover:bg-red-800 hover:ring-0 hover:border-red-800 focus:bg-red-800 focus:border-red-800 focus:outline-none focus:ring-0"
+                            className="py-3 px-5 -ml-1 rounded-md leading-5 text-red-800 bg-red-800 border border-red-700 hover:text-white hover:bg-red-800 hover:ring-0 hover:border-red-800 focus:bg-red-800 focus:border-red-800 focus:outline-none focus:ring-0"
                             type="submit"
                           >
                             <svg
@@ -132,7 +145,7 @@ export default function Contact() {
                               width="1.5rem"
                               height="1.5rem"
                               fill="currentColor"
-                              class="ltr:mr-2 rtl:ml-2 tranform ltr:rotate-0 rtl:-rotate-90 inline-block"
+                              className="ltr:mr-2 rtl:ml-2 tranform ltr:rotate-0 rtl:-rotate-90 inline-block"
                               viewBox="0 0 512 512"
                             >
                               <path d="M53.12,199.94l400-151.39a8,8,0,0,1,10.33,10.33l-151.39,400a8,8,0,0,1-15-.34L229.66,292.45a16,16,0,0,0-10.11-10.11L53.46,215A8,8,0,0,1,53.12,199.94Z"></path>
